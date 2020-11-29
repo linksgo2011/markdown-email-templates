e8ba8a0125e07dbf080fbd3b2fa20d144bcfb0c4
lin <120377843@qq.com>
2020-11-29 16:51:47 +0800

    add two templates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# markdown-email-templates
-email templates for mardown
+# 使用 markdown 编写邮件博客
+
+使用 markdown 可以简单的方便的编写内容，然后生成排版良好的的邮件。原理是通过 pandoc 软件将 markdown 转换成 HTML 页面，不过 HTML 需要特别处理以兼容大多数邮件客户端。
+
+邮件能正常渲染的 HTML 页面只能使用 table 布局，并支持有限的 css 样式。
---
+++ buildemail.sh
@@ -0,0 +1,11 @@
+file=README.md
+template=templates/center-align.html
+imageHost=https://github.com/linksgo2011/markdown-email-templates/raw/master/
+
+rm -rf build
+mkdir build
+
+pandoc -s $file -w html --template=$template -o build/email.html --title-prefix email
+
+# 替换图片相对连接为绝对连接
+sed -i "" "s@src=\"@src=\"$imageHost@g" build/email.html
